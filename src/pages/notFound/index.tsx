d60056947fa8aed2258b547878a23879664f71c5
less <zhangmingcheng0412@gmail.com>
2022-06-13 18:12:42 +0800

    refactor:add 404 page

---
+++ src/pages/notFound/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+type NotFoundProps = {
+
+}
+
+const NotFound:React.FC<NotFoundProps> = () => {
+ return (
+  <div>
+   404 page....
+  </div>
+ );
+};
+
+export default NotFound;
