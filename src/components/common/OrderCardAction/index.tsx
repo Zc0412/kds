4a8eb753f3d3a9a2842225722a26cb1b9fc6404b
less <zhangmingcheng0412@gmail.com>
2022-06-16 10:08:38 +0800

    refactor:添加OrderCardAction组件

---
+++ src/components/common/OrderCardAction/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from "./OrderCardAction.module.css";
+import {Button} from "@mui/material";
+import finishedBlue from "../../../assets/images/finished_blue@2x.png";
+import finishedWhite from "../../../assets/images/finished_white@2x.png";
+import {OrderCardLevel} from "../../../types/typing";
+
+type OrderCardActionProps = {
+  disabled?: boolean
+} & OrderCardLevel
+
+const OrderCardAction: React.FC<OrderCardActionProps> = ({level = 'high', disabled = false}) => {
+  // 订单紧急程度返回的按钮颜色值
+  const textColor = level === "low" ? '#30383D' : '#FFFFFF'
+  // icon
+  const startIcon = level === "low" ? finishedBlue : finishedWhite
+
+  return (
+    <div className={styles['order-card-action']}>
+      <Button
+        disabled={disabled}
+        fullWidth
+        sx={{color: textColor}}
+        startIcon={<img className={styles['order-card-action-icon']} src={startIcon} alt='finished' />}
+      >
+        Done
+      </Button>
+    </div>
+  );
+};
+
+export default OrderCardAction;
