bee718ea8742123b16ffdd129ae75051ded133d8
less <zhangmingcheng0412@gmail.com>
2022-06-21 15:59:39 +0800

    refactor:添加字体，修改默认宽度

--- src/components/common/OrderCardDrag/index.tsx
+++ src/components/common/OrderCardDrag/index.tsx
@@ -10,20 +10,20 @@ type OrderCardDragProps = {
 export const DragContext = createContext<any>(null!)
 
 const OrderCardDrag: React.FC<OrderCardDragProps> = ({enabled = false, children}) => {
-  const [{x, y, scale}, api] = useSpring(() => ({x: 0, y: 0, scale: 1}))
+  const [{x, y, scale, zIndex}, api] = useSpring(() => ({x: 0, y: 0, scale: 1, zIndex: 0}))
   const bind = useDrag(
     ({down, movement: [mx, my]}) => {
       if (mx < -50) {
         console.log('done')
         console.log(mx)
       }
-      api.start({x: down ? mx : 0, y: down ? my : 0, scale: down ? 1.02 : 1})
+      api.start({x: down ? mx : 0, y: down ? my : 0, scale: down ? 1.02 : 1, zIndex: down ? 1 : 0})
     },
     {enabled: enabled}
   )
   return (
     <DragContext.Provider value={bind()}>
-      <animated.div style={{x, y, scale, touchAction: "none"}}>
+      <animated.div style={{x, y, scale, zIndex, touchAction: "none"}}>
         {children}
       </animated.div>
     </DragContext.Provider>
