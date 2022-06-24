735f2ed576efad97198daa5d6ed6e79d76b23af0
less <zhangmingcheng0412@gmail.com>
2022-06-24 14:53:40 +0800

    refactor:设置数据接口URL

--- src/constant/common.ts
+++ src/constant/common.ts
@@ -1,1 +1,5 @@
+// 正则校验
 export const USER_NAME_REGEXP: RegExp = /^[0-9]{10}$/
+
+// 请求url
+export const BASE_URL = process.env.NODE_ENV==='development'?'https://mock.apifox.cn/m1/1135409-0-default/':''
