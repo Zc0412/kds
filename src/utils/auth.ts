b0a9f5c3612ab85f53e9f37eb4ebae41d2a58147
less <zhangmingcheng0412@gmail.com>
2022-06-24 14:53:08 +0800

    refactor:修改命名

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,16 +1,16 @@
 /**
  * This represents some generic auth provider API, like Firebase.
  */
-const fakeAuthProvider = {
+const authProvider = {
   isAuthenticated: false,
   signin(callback: VoidFunction) {
-    fakeAuthProvider.isAuthenticated = true;
+    authProvider.isAuthenticated = true;
     setTimeout(callback, 100); // fake async
   },
   signout(callback: VoidFunction) {
-    fakeAuthProvider.isAuthenticated = false;
+    authProvider.isAuthenticated = false;
     setTimeout(callback, 100);
   },
 };
 
-export { fakeAuthProvider };
+export {authProvider};
